0d3f9458fa40672906833f26a80fe96d1e86ccf2
Manipriya <pupppalamanipriya@gmail.com>
2025-09-22 14:16:09 +0530

    funtion

---
+++ funtion.js
@@ -0,0 +1,23 @@
+// function add(){
+// var a=34;
+// var b=23;
+// document.write(a+b);
+// console.log(a+b);
+// }
+
+// add();
+
+//es6
+// const add =() => {
+//     var a=45;
+//     var b=58;
+//     document.write(a+b);
+//     console.log(a+b);
+// };
+// add();
+
+//es7
+// const add=(a,b) => console.log(a+b);
+// add(34,33);
+//  console.log(b);
+//  b=35;
